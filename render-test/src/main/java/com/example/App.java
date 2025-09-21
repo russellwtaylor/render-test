cd3886a5686a6c318e54551d8a4bda87705cb502
Russ Taylor <rssell.taylor@gmail.com>
2025-09-21 17:52:32 -0400

    update endpoint with counter

--- render-test/src/main/java/com/example/App.java
+++ render-test/src/main/java/com/example/App.java
@@ -1,13 +1,24 @@
 package com.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World! It's me, Render" );
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class App {
+
+    private Integer count = 0;
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+
+    @GetMapping("/hello")
+    public String hello() {
+        this.count++;
+        System.out.println("Called hello: " + this.count);
+        return "hello world";
     }
 }
